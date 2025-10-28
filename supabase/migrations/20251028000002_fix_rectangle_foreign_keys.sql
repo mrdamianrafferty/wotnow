2a834b64b1de5e89d37c5b188c0657e8f383241b
Damian Rafferty <damian@flyglobalmusic.com>
2025-10-28 16:47:55 +0100

    Clean up orphaned records before adding foreign keys

--- supabase/migrations/20251028000002_fix_rectangle_foreign_keys.sql
+++ supabase/migrations/20251028000002_fix_rectangle_foreign_keys.sql
@@ -12,6 +12,19 @@ DROP CONSTRAINT IF EXISTS fk_catch_entries_rectangle;
 ALTER TABLE findr_fishing_sessions
 DROP CONSTRAINT IF EXISTS fk_fishing_sessions_rectangle;
 
+-- Clean up orphaned records before adding correct foreign keys
+-- Delete impressions with rectangle codes that don't exist in ices_rectangles
+DELETE FROM findr_prediction_impressions
+WHERE rectangle_code NOT IN (SELECT rectangle_code FROM ices_rectangles);
+
+-- Delete catches with rectangle codes that don't exist in ices_rectangles
+DELETE FROM findr_catch_entries
+WHERE rectangle_code NOT IN (SELECT rectangle_code FROM ices_rectangles);
+
+-- Delete sessions with rectangle codes that don't exist in ices_rectangles
+DELETE FROM findr_fishing_sessions
+WHERE rectangle_code NOT IN (SELECT rectangle_code FROM ices_rectangles);
+
 -- Recreate foreign keys pointing to the correct ices_rectangles table
 ALTER TABLE findr_prediction_impressions
 ADD CONSTRAINT fk_prediction_impressions_rectangle
