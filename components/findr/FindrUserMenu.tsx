95e51d849b1da43edcbd1ea0c6678e6e8edca784
Damian Rafferty <damian@flyglobalmusic.com>
2025-11-05 18:36:31 +0100

    Fix avatar crop: remove inline style, use Tailwind for circular image

--- components/findr/FindrUserMenu.tsx
+++ components/findr/FindrUserMenu.tsx
@@ -63,7 +63,6 @@ export default function FindrUserMenu() {
               width={40}
               height={40}
               className="w-full h-full object-cover"
-              style={{ width: 'auto', height: 'auto', maxWidth: '100%', maxHeight: '100%' }}
               unoptimized // OAuth avatars are already optimized
             />
           ) : (
