1f17ca13ffe870775e4d11566a314cab5c409b79
Damian Rafferty <damian@flyglobalmusic.com>
2025-12-17 14:38:43 +0100

    fix: add Supabase storage hostname to Next.js image config

--- next.config.mjs
+++ next.config.mjs
@@ -158,6 +158,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'swmviqpxetwziqxhzldh.supabase.co',
+        port: '',
+        pathname: '/storage/**',
+      },
     ],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
