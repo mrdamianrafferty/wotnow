47fad949095fea9cbc6886bee2b2ba4821373202
Damian Rafferty <damian@flyglobalmusic.com>
2025-12-26 17:29:29 +0100

    chore(species): allow explicit any for build-time fetching (temporary)

--- pages/findr/species/[slug].tsx
+++ pages/findr/species/[slug].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import SEO from '../../../components/SEO';
