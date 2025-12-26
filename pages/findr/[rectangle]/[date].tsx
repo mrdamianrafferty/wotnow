18c31747ee3e532817998ead58997d660bce93af
Damian Rafferty <damian@flyglobalmusic.com>
2025-12-26 15:56:46 +0100

    fix(sitemap): remove unused import to satisfy lint

--- pages/findr/[rectangle]/[date].tsx
+++ pages/findr/[rectangle]/[date].tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { GetServerSideProps } from 'next';
 import SEO from '../../../components/SEO';
-import { useRouter } from 'next/router';
 
 interface Props {
   rectangle: string;
